d2d5be73c0eacdf056c71c35b2032a5575c2dba7
Raj12222 <105064330+Raj12222@users.noreply.github.com>
2022-07-23 14:52:34 +0530

    Add files via upload

---
+++ raj.js
@@ -0,0 +1,24 @@
+function externalalert(){
+    alert("Hello Guys I'm Raj.");
+}
+
+
+function externalconfirm(){
+    if(confirm("Are You Sure????"))
+    {
+        alert("Yesssss");
+
+    }
+    else
+    {
+        alert("Noooooo");
+    }
+}
+
+function externalpromt(){
+
+    var fname=prompt("Enter Your First Name Here");
+    var lname=prompt("Enter Your Last Name Here");
+    alert(fname +" "+ lname);
+
+}
